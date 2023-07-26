b53970c8440505264f14d80851e359827b7e73e9
drejmin <djjamn@gmail.com>
2023-07-26 10:31:14 -0700

    modified:   models/classes.js 	modified:   controllers/classes.js

--- models/classes.js
+++ models/classes.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const classesSchema = new Schema({
-  subject: String,
+  subject: {type:String, required: true},
   courseName: String,
   teacherName: String,
   private: Boolean,
