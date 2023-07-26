746e353c4a072bb24e46f3fc653702a82afdb79e
drejmin <djjamn@gmail.com>
2023-07-26 12:11:37 -0700

    modified:   routes/classes.js 	modified:   models/notes.js 	modified:   routes/groups.js

---
+++ routes/classes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+// You'll be creating this controller module next
+const classesCtrl = require('../controllers/classes');
+	
+// GET /notes
+router.get('/classes', classesCtrl.index);
+// GET /notes/new
+router.get('/classes/new', classesCtrl.new);
+// GET /notes/:id (show functionality) MUST be below new route
+router.get('/classes/:id', classesCtrl.show);
+// DELETE /notes/:id
+//router.get('/:id', classesCtrl.delete);
+// POST /notes
+router.post('/classes', classesCtrl.create);
+	
+module.exports = router;
