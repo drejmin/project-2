20ed74af72e32bfab679da7afcf2bbfd8c2140c1
drejmin <djjamn@gmail.com>
2023-09-11 10:19:06 -0700

    modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ const notesRouter = require('./routes/notes');
 
 const app = express();
 
+// mongoURI setup
+const mongoURI = process.env.DATABASE_URL;
+
+app.set("port", process.env.PORT || 8000);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
